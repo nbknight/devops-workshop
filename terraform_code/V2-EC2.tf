660ddc2e5666b559b5346e94e27b97342721a143
Nick Knight <nick.knight@riaadvisory.com>
2023-11-03 15:26:40 -0400

    V2 version of Ec2 with updates

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "demo_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
